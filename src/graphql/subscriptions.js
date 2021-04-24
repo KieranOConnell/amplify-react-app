d4b6d4cf0f416517c92ee8f2c718fb76ae196d24
Kieran O'Connell <kieran.oconnell@students.ittralee.ie>
2021-04-24 21:27:56 +0100

    Added GraphQL API using AWS AppSync

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateImage = /* GraphQL */ `
+  subscription OnCreateImage {
+    onCreateImage {
+      id
+      location
+      photographer
+      filePath
+      likes
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateImage = /* GraphQL */ `
+  subscription OnUpdateImage {
+    onUpdateImage {
+      id
+      location
+      photographer
+      filePath
+      likes
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteImage = /* GraphQL */ `
+  subscription OnDeleteImage {
+    onDeleteImage {
+      id
+      location
+      photographer
+      filePath
+      likes
+      createdAt
+      updatedAt
+    }
+  }
+`;
